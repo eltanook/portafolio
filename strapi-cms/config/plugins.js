92ac934c083e6466cae8ba63342de13dd4b0c576
eltanook <tomasnadal04@gmail.com>
2025-10-26 16:29:17 -0300

    Disable cronjob completely

--- strapi-cms/config/plugins.js
+++ strapi-cms/config/plugins.js
@@ -1,10 +1,5 @@
 module.exports = ({ env }) => ({
   'cloud-cronjob-runner': {
-    enabled: true,
-    config: {
-      apiToken: env('STRAPI_CLOUD_API_TOKEN', 'dummy-token'),
-      apiUrl: env('STRAPI_CLOUD_API_URL', 'https://cloud.strapi.io'),
-      firstRunWindow: env.int('CRONJOB_FIRST_RUN_WINDOW', 60),
-    },
+    enabled: false,
   },
 });
